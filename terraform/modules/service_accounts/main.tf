c04f073300fd2a8601e75a4eb34f0bd79ea602b2
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-04-16 17:51:01 +0530

    Service Account Update

--- terraform/modules/service_accounts/main.tf
+++ terraform/modules/service_accounts/main.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "service_account" {
 
 resource "google_service_account_iam_member" "roles" {
   for_each           = toset(var.roles)
-  service_account_id = google_service_account.service_account.account_id
+  service_account_id = google_service_account.service_account.id
   role               = each.key
   member             = "serviceAccount:${google_service_account.service_account.email}"
 }
