6dfbdbde5bdd9bf932fd9a8e500c9247dcf8d67d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-04-16 17:51:28 +0530

    Kubernetes Network Configuration Added

---
+++ terraform/modules/k8s/network/outputs.tf
@@ -0,0 +1,24 @@
+output "network_name" {
+  value       = google_compute_network.vpc.name
+  description = "VPC Network Name"
+}
+
+output "subnetwork_name" {
+  value       = google_compute_subnetwork.subnet.name
+  description = "VPC Subnet Name"
+}
+
+output "cluster_control_plane_ip_cidr_range" {
+  value       = local.cluster_control_plane_ip_cidr_range
+  description = "Cluster Control Plane IP CIDR Range"
+}
+
+output "cluster_pods_ip_cidr_range" {
+  value       = local.cluster_pods_ip_cidr_range
+  description = "Cluster Pods IP CIDR Range"
+}
+
+output "cluster_services_ip_cidr_range" {
+  value       = local.cluster_services_ip_cidr_range
+  description = "Cluster Services IP CIDR Range"
+}
