939759208912822b0e1e3f722132142b3b42ad73
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-11-19 16:40:46 +0530

    Firebase Project Creation

---
+++ terraform/infrastructure/firebase-project/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "1.3.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.61.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.61.0"
+    }
+  }
+  backend "gcs" {
+    prefix = "firebase-projects"
+  }
+}
