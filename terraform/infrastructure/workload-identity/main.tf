f1b4f930b9621b6f159655d3d378a9a41cf2e113
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-11-23 17:12:13 +0530

    Infrastructure for WIF Added

---
+++ terraform/infrastructure/workload-identity/main.tf
@@ -0,0 +1,7 @@
+module "gh_oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id  = var.project_id
+  pool_id     = var.pool_id
+  provider_id = var.provider_id
+  sa_mapping  = var.sa_mapping
+}
